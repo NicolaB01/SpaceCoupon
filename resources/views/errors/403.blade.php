137222bb811b22865deae41d2e91d1404110c8a0
Nicola <s1099740@studenti.univpm.it>
2023-06-08 20:32:57 +0200

    http 404 e 403

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,15 +1,22 @@
-@extends('layout/body')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SpaceCoupon.com</title>
 
-@section('scripts')
-<script src="{{ asset('js/errors.js') }}" defer></script>
-@endsection
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
-@section('content')
-<div class="wrapper-message-error">
-    <p class="text_403">4 
-      <i class="fa fa-user-astronaut"></i> 
-      3</p>
-    <p class="text_lost">Non sei autorizzato</p>
-    <a href="">Torna alla home</a>
-</div>
-@endsection
+  <link rel="stylesheet" type="text/css" href="{{asset('css/error.css')}}">
+</head>
+<body>
+  <div class="wrapper-message-error">
+      <p class="text_403">4 
+        <i class="fa fa-user-astronaut"></i> 
+        3</p>
+      <p class="text_lost">Non sei autorizzato</p>
+      <a href="{{route('home')}}">Torna alla home</a>
+  </div>
+</body>
+</html>
