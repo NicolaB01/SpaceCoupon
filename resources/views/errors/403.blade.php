f7b089a2f9ec9832e38b09dc75eabd9b2ede8c68
Tommaso <s1100538@studenti.univpm.it>
2023-06-08 21:48:51 +0200

    fix

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -8,6 +8,7 @@
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
+  <link rel="stylesheet" type="text/css" href="{{asset('css/index.css')}}">
   <link rel="stylesheet" type="text/css" href="{{asset('css/error.css')}}">
 </head>
 <body>
